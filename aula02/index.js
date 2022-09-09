e202841d97bd1cefa5109821455c6d66370f55ab
Enzo Fagundes <enzo.fagundes014@gmail.com>
2022-09-09 18:14:34 -0300

    adicionando os exercícios de mais duas aulas

---
+++ aula02/index.js
@@ -0,0 +1,30 @@
+//Entendendo sobre a função console.log
+//Log é um método que se encontra dentro do objeto console
+
+//Pode receber variáveis, métodos, funções, objetos etc.
+
+//String
+console.log('Hello, world!'); //Aspas simples
+console.log("How are you?"); //Aspas duplas
+console.log(`You're welcome`); //Cráse
+
+/*
+    Terminal:
+
+    Hello, world!
+    How are you?
+    You're welcome
+
+    O comportamento padrão é o de quebrar as linhas
+ */
+
+//Números
+
+console.log(1); //Números inteiros
+console.log(2.35); //Números reais
+console.log('12'); //Isso está errado, o tipo desse valor é string
+
+//Concatenando valores
+
+console.log(32, 14.44, 'Wassup?'); //Utilizando vírgula
+console.log(32 + 14.44 + "So, it's all ours?"); //Utilizando o mais
---
+++ aula03/index.js
@@ -0,0 +1,5 @@
+//Exercício:
+//Apresentar a seguinte mensagem através do console.log
+//Meu nome é "NOME". Estou aprendendo JavaScript às 18h da tarde.
+
+console.log('Meu nome é "Enzo".', 'Estou aprendendo JavaScript às', 18, 'horas da tarde.');
