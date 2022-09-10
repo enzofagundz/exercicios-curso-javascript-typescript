519cc8dfcac6a957efbe98efe1b98e75264fdbb9
Enzo Fagundes <enzo.fagundes014@gmail.com>
2022-09-10 14:52:11 -0300

    correção do exercício 3 e criação do 4

--- aula03/index.js
+++ aula03/index.js
@@ -2,4 +2,4 @@
 //Apresentar a seguinte mensagem através do console.log
 //Meu nome é "NOME". Estou aprendendo JavaScript às 18h da tarde.
 
-console.log('Meu nome é "Enzo".', 'Estou aprendendo JavaScript às', 18, 'horas da tarde.');
+console.log('Meu nome é "Enzo". Estou aprendendo JavaScript às', 18, 'horas da tarde.');
---
+++ aula04/comentarios.js
@@ -0,0 +1,30 @@
+//Comentários são trechos de código ignorados pelo interpretador javascript
+//Eles funcionam como anotações em um código JS
+
+//Para comentar em linha única devemos utilizar as duas barras -> //
+//Ou podemos utilizar a barra acompanhada de um asterisco para realizar comentários em bloco. Desta forma:
+
+/*
+    Este é um exemplo de comentário em bloco
+*/
+
+//Testandp na prática como funciona um comentário:
+
+console.log('Linha 1');
+
+//console.log('Linha 2');
+
+console.log('Linha 3');
+
+/*
+console.log('Linha 4');
+console.log('Linha 5');
+*/
+
+//Desta forma, a saída do console foi:
+/*
+    Linha 1
+    Linha 3 
+*/
+
+//Nota-se que as linhas 15, 19, 20, 21, 22 foram ignoradas
